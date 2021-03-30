16120c5a3d62a063970c52c9703258dd5f9dc292
Dale Floer <dalefloer@gmail.com>
2021-03-30 16:49:06 -0700

    WiP security for rabbitmq.

--- django/message_queue/tasks.py
+++ django/message_queue/tasks.py
@@ -6,6 +6,8 @@ from sg_management.models import SensorGnome
 import kombu
 import json
 import datetime
+from django.db import transaction
+
 
 os.environ.setdefault("EVENT_CONSUMER_APP_CONFIG", "sensorgnome_server.settings")
 
@@ -18,12 +20,18 @@ from event_consumer import message_handler
 
 @message_handler(routing_keys='sensorgnome-management')
 def process_message(body):
-    j = json.loads(body)
-    print(f"Message from {j['id']}: \"{j['message']}\".")
-    for k in j.keys():
-        if k not in ("id", "message"):
-            print(f"Extra: {k}: {j[k]}.")
-    sg = SensorGnome.objects.get(serial=j["id"])
-    m = Message(payload=j["message"], sensorgnome=sg)
-    m.save()
-    sg.update_last_seen()  # Would be nice if the last two lines could be atomic.
+    try:
+        j = json.loads(body)
+        print(f"Message from {j['id']}: \"{j['message']}\".")
+        for k in j.keys():
+            if k not in ("id", "message"):
+                print(f"Extra: {k}: {j[k]}.")
+        sg = SensorGnome.objects.get(serial=j["id"])
+        m = Message(payload=j["message"], sensorgnome=sg)
+        with transaction.atomic():
+            m.save()
+            sg.update_last_seen()
+        return True
+    except Exception as e:
+        print("Exception:", e)
+        return True
